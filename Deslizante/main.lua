0a332575af320291f175f4fd5cbd872763375966
Gabriel Banaggia <gbanaggia@users.noreply.github.com>
2020-07-03 17:04:58 -0300

    Create main.lua
    Estrutura para máquina de estados do jogo

---
+++ Deslizante/main.lua
@@ -0,0 +1,49 @@
+-- definindo estados do jogo
+local abertura = true
+local selecionando = false
+local resolvendo = false
+local terminado = false
+
+function love.load ()
+    love.window.setMode (1000,1000)
+    love.window.setTitle ("Deslize")
+    love.graphics.setBackgroundColor (0.95, 0.95, 0.95) -- quase branco
+    w, h = love.graphics.getDimensions ()
+    math.randomseed (os.time())
+end
+
+function love.keypressed (key)
+    if key == 'enter' then
+        if abertura then
+            abertura = false
+            selecionando = true
+        end
+    end
+    if key == 'escape' then
+        love.event.quit()
+    end
+    if key == 'r' then
+        love.event.quit('restart')
+    end
+end
+
+function love.mousepressed (x, y, bt)
+    if bt ~= 1 then return -- verifica se é botão esquerdo
+    end 
+    if selecionando then
+        --[[
+        ...clique para escolha da dificuldade
+        selecionando = false
+        ...chamada da função para embaralhar
+        resolvendo = true
+        --]]
+    end
+end
+
+function love.update (dt)
+    if resolvendo then
+        --[[
+        ...função para verificar se jogo terminou, retorna resolvendo = false e terminado = true
+        --]]
+    end
+end
